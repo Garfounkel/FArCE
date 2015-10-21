03292d26273a06cfd7073f83446c17e3074645f2
St0rmingBr4in <julien.doche@gmail.com>
2015-10-21 12:29:50 +0200

    add Preprocessing (pas de points d'exclamations ¡¡¡¡)

---
+++ src/Preprocessing/Image_OPs.c
@@ -0,0 +1,39 @@
+
+# include "Image_OPs.h"
+
+SDL_Surface* to_grey(SDL_Surface *img)
+{
+  for(int h = 0; h < img->h; h++)
+  {
+    for(int w = 0; w < img->w; w++)
+    {
+      Uint8 r;
+      Uint8 g;
+      Uint8 b;
+      SDL_GetRGB(getpixel(img,w,h), img->format, &r, &g, &b);
+
+      Uint8 m = 0.3 * r + 0.59 * g + 0.11 * b;
+
+      putpixel(img,w,h,SDL_MapRGB(img->format, m, m, m));
+
+    }
+  }
+  return img;
+}
+
+SDL_Surface* invert_grey(SDL_Surface *img)
+{
+  for (int h = 0; h < img->h; h++)
+  {
+    for (int w = 0; w < img->w; w++)
+    {
+      Uint8 m;
+      SDL_GetRGB(getpixel(img,w,h),img->format,&m,&m,&m);
+
+      m = -m + 255;
+
+      putpixel(img,w,h,SDL_MapRGB(img->format, m, m, m));
+    }
+  }
+  return img;
+}
---
+++ src/Preprocessing/Image_OPs.h
@@ -0,0 +1,12 @@
+
+# ifndef IMAGE_OPS_H_
+# define IMAGE_OPS_H_
+
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include "../ViolaJones/pixel_operations.h"
+
+SDL_Surface* to_grey(SDL_Surface *img);
+SDL_Surface* invert_grey(SDL_Surface *img);
+
+# endif
