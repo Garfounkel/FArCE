2e21d7bee3a6cfc7e1fb678b201926ffea45ab64
St0rmingBr4in <julien.doche@gmail.com>
2015-10-31 20:57:08 +0100

    add adaboost

---
+++ src/ViolaJones/adaboost.h
@@ -0,0 +1,12 @@
+# ifndef ADABOOST_H_
+# define ADABOOST_H_
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include <err.h>
+# include "pixel_operations.h"
+# include "Ulong_tab.h"
+
+# endif
