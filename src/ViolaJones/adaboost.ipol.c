6e4e59c168cef1e68641061bfc7f3a32d6bebd6a
St0rmingBr4in <julien.doche@gmail.com>
2015-11-30 23:06:19 +0100

    add ipol algorithm 4 not finished

---
+++ src/ViolaJones/adaboost.ipol.c
@@ -0,0 +1,19 @@
+// Caracteristique = (threshold, toggle, error, margin)
+
+
+// algorithme 4
+
+Caracteristique find_Decision_Stump(training_examples* n_training_examples, weight* probabilistic_example_weights)
+{
+
+  threshold t = min(n_training_examples);
+  margin M = 0;
+  error e = 2;
+
+  //Sum up the weights of the positive (resp. negative) examples whose f-th feature is bigger than the present threshold
+
+  weight Wp1 = sum();
+
+
+
+}
