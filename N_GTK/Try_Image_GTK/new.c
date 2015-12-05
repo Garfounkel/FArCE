eb92de6ce87daddea0431efb92b3586dd57750ea
Benoit Frederic-moreau <freder_b@epita.fr>
2015-12-05 15:47:33 +0100

    NONO iS FOR YOU

--- N_GTK/Try_Image_GTK/new.c
+++ N_GTK/Try_Image_GTK/new.c
@@ -4,24 +4,47 @@
 
 static GtkWidget* Picture =NULL;
 static GtkWidget* image;
+
+
+typedef struct Screen Screen;
+struct Screen
+{
+ GtkWidget* Window; 
+ GtkWidget* image; 
+};
+
+  
 void destroy(void) {
   gtk_main_quit();
 }
 
-void choose(void) {
-  
-  Picture =  gtk_image_new_from_file("rees_a");
+void save(void) {
+  gtk_main_quit();
+}
+
+void supp(void) {
+  gtk_main_quit();
+}
+
+GtkWidget* choose(void) {
+
+  Picture =  gtk_image_new_from_file("rees_a");  
+  return Picture;
 }
 
 
-
-
-int main (int argc, char** argv) {
-  //char *title = argv[1];
+Screen* init_screen(GtkWidget* title)
+{
+  Screen *screen=malloc(sizeof(struct Screen));
+  screen->image = title;
+  screen->Window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  return screen;
   
-  GtkWidget* window;  
+}
+
+int main (int argc, char** argv) {   
+  // GtkWidget* window;  
   // GtkWidget* image;
-  
   //GtkWidget *Button;
   GtkWidget *MenuBar;
   GtkWidget *Menu;
@@ -29,22 +52,25 @@ int main (int argc, char** argv) {
   GtkWidget *MenuItem;
   // GtkWidget *VBox;
   GtkWidget *Image_box;
+   
+  gtk_init (&argc, &argv);
+  
  
   
-  gtk_init (&argc, &argv);
-  //WINDOW
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-
-  //post Window
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 200,200); 
+  ////////////// WINDOW/////////////////
+  // window = gtk_window_new(GTK_WINDOW_TOPLEVEL);  
+  Screen *scre = init_screen(gtk_image_new_from_file(argv[1]));
+  
+  ////////////post Window/////////////////
+  gtk_window_set_position(GTK_WINDOW(scre->Window), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(scre->Window), 320,200); 
  
   //image
   Image_box = gtk_vbox_new(FALSE, 5 );
-  gtk_container_add(GTK_CONTAINER(window),Image_box);
+  gtk_container_add(GTK_CONTAINER(scre->Window),Image_box);
   image = gtk_image_new_from_file(argv[1]);
   // gtk_box_pack_start(GTK_BOX(Image_box), image,FALSE,TRUE,0);
- 
+
  
   MenuBar = gtk_menu_bar_new();
   Menu = gtk_menu_new();
@@ -53,7 +79,21 @@ int main (int argc, char** argv) {
    
   MenuItem = gtk_menu_item_new_with_label("Choose");
   gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
+  gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",  G_CALLBACK(choose), NULL);
+  
+ if(Picture != NULL)
+    {image = Picture;}
+
+    
+  MenuItem = gtk_menu_item_new_with_label("Save");
+  gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
+  gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",  GTK_SIGNAL_FUNC(save), NULL);
+
+    
+  MenuItem = gtk_menu_item_new_with_label("Supp");
+  gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
   gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",  GTK_SIGNAL_FUNC(choose), NULL);
+  
   
   MenuItem = gtk_menu_item_new_with_label("Analyse");
   gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
@@ -67,8 +107,8 @@ int main (int argc, char** argv) {
   ///////////////////////////////////////////////////////
 
 
-  if(Picture != NULL)
-    {image = Picture;}
+  /* if(Picture != NULL)
+     {image = Picture;}*/
   
    //ADD Menu
   MenuItem = gtk_menu_item_new_with_label("File");
@@ -79,11 +119,11 @@ int main (int argc, char** argv) {
   
   // Menu au dessu image//
   gtk_box_pack_start(GTK_BOX(Image_box), MenuBar, FALSE, FALSE, 0);  
-  gtk_box_pack_start(GTK_BOX(Image_box), image,FALSE,TRUE,0);
+  gtk_box_pack_start(GTK_BOX(Image_box),scre->image,TRUE,FALSE,0);
     
-  gtk_signal_connect(GTK_OBJECT (window), "destroy", GTK_SIGNAL_FUNC (destroy), NULL);
+  gtk_signal_connect(GTK_OBJECT (scre->Window), "destroy", GTK_SIGNAL_FUNC (destroy), NULL);
  
-  gtk_widget_show_all(window);
+  gtk_widget_show_all(scre->Window);
   gtk_main();
   //return EXIT_SUCCESS;
   return 0;
