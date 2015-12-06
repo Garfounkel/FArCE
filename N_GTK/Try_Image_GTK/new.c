5b6fea7e7b50cbe1014646be4adfe55e1d6333bc
Benoit Frederic-moreau <freder_b@epita.fr>
2015-12-06 12:11:57 +0100

    folder 2

--- N_GTK/Try_Image_GTK/new.c
+++ N_GTK/Try_Image_GTK/new.c
@@ -1,7 +1,7 @@
 # include <gtk/gtk.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include "test.h"
+
 
 
 typedef struct Screen Screen;
@@ -62,9 +62,11 @@ void Navigate(){
     //On interdit l'utilisation des autres fenêtres.
     gtk_window_set_modal(GTK_WINDOW(selection), TRUE);
      
-    g_signal_connect(G_OBJECT(GTK_FILE_SELECTION(selection)->ok_button), "clicked", G_CALLBACK(recuperer_chemin), selection );
+    g_signal_connect(G_OBJECT(GTK_FILE_SELECTION(selection)->ok_button), "clicked",
+		     G_CALLBACK(recuperer_chemin), selection );
      
-    g_signal_connect_swapped(G_OBJECT(GTK_FILE_SELECTION(selection)->cancel_button), "clicked", G_CALLBACK(gtk_widget_destroy), selection);
+    g_signal_connect_swapped(G_OBJECT(GTK_FILE_SELECTION(selection)->cancel_button), "clicked",
+		     G_CALLBACK(gtk_widget_destroy), selection);
 }
 
 
@@ -82,13 +84,14 @@ int main (int argc, char** argv) {
  
   GtkWidget *MenuBar;
   GtkWidget *Menu;
-  // GtkWidget *button;
+  GtkWidget *StatusBar;
   GtkWidget *MenuItem;
   GtkWidget *Image_box;
    
   gtk_init (&argc, &argv);
    
   ////////////// WINDOW/////////////////
+  
   // window = gtk_window_new(GTK_WINDOW_TOPLEVEL);  
   Screen *scre = init_screen(gtk_image_new_from_file(argv[1]));
   
@@ -102,7 +105,10 @@ int main (int argc, char** argv) {
   /////////////image/////////////
   Image_box = gtk_vbox_new(FALSE, 0 );
   gtk_container_add(GTK_CONTAINER(scre->Window),Image_box);
-  
+  //////Photo title
+  StatusBar = gtk_statusbar_new();
+  gtk_statusbar_push (GTK_STATUSBAR (StatusBar), 0,"NONO");
+    
   MenuBar = gtk_menu_bar_new();
   Menu = gtk_menu_new();
 
@@ -142,9 +148,8 @@ int main (int argc, char** argv) {
   gtk_menu_shell_append(GTK_MENU_SHELL(MenuBar), MenuItem);
   
   // Menu au dessus image//
-  gtk_box_pack_start(GTK_BOX(Image_box), MenuBar, FALSE, FALSE, 0);  
-  gtk_box_pack_start(GTK_BOX(Image_box),scre->image,TRUE,FALSE,0);
-  //  gtk_box_pack_end(GTK_BOX(Image_box),button,FALSE,FALSE,0);  
+  gtk_box_pack_start(GTK_BOX(Image_box), MenuBar, FALSE, FALSE, 0);  gtk_box_pack_start(GTK_BOX(Image_box),scre->image,TRUE,FALSE,0);
+  gtk_box_pack_end(GTK_BOX(Image_box), StatusBar, FALSE, FALSE, 0);
 
   gtk_signal_connect(GTK_OBJECT (scre->Window), "destroy", GTK_SIGNAL_FUNC (Destroy), NULL);
  
