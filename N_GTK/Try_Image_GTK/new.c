953d32fa162621a8766c35fd8831b40120052041
Benoit Frederic-moreau <freder_b@epita.fr>
2015-12-06 13:05:06 +0100

    NONO success #dancedelajoie

--- N_GTK/Try_Image_GTK/new.c
+++ N_GTK/Try_Image_GTK/new.c
@@ -11,6 +11,7 @@ struct Screen
  GtkWidget* image;
   
 };
+static Screen *scre;
 
   
 void Destroy(void) {
@@ -47,7 +48,9 @@ void recuperer_chemin(GtkWidget *bouton, GtkWidget *file_selection)
     GTK_MESSAGE_INFO,
     GTK_BUTTONS_OK,
     "Vous avez choisi :\n%s", chemin);
-     
+    
+    gtk_image_set_from_file(GTK_IMAGE(scre->image),chemin);
+
     gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
     gtk_widget_destroy(file_selection);
@@ -63,7 +66,7 @@ void Navigate(){
     gtk_window_set_modal(GTK_WINDOW(selection), TRUE);
      
     g_signal_connect(G_OBJECT(GTK_FILE_SELECTION(selection)->ok_button), "clicked",
-		     G_CALLBACK(recuperer_chemin), selection );
+		     G_CALLBACK(recuperer_chemin), selection);
      
     g_signal_connect_swapped(G_OBJECT(GTK_FILE_SELECTION(selection)->cancel_button), "clicked",
 		     G_CALLBACK(gtk_widget_destroy), selection);
@@ -92,35 +95,29 @@ int main (int argc, char** argv) {
    
   ////////////// WINDOW/////////////////
   
-  // window = gtk_window_new(GTK_WINDOW_TOPLEVEL);  
-  Screen *scre = init_screen(gtk_image_new_from_file(argv[1]));
+   scre = init_screen(gtk_image_new_from_file(argv[1]));
   
   ////////////post Window/////////////////
   gtk_window_set_position(GTK_WINDOW(scre->Window), GTK_WIN_POS_CENTER);
   gtk_window_set_default_size(GTK_WINDOW(scre->Window), 320,200); 
 
-  //  button = gtk_button_new_with_mnemonic("_gggg");
-   //  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(Navigate),NULL);
   
   /////////////image/////////////
   Image_box = gtk_vbox_new(FALSE, 0 );
   gtk_container_add(GTK_CONTAINER(scre->Window),Image_box);
   //////Photo title
   StatusBar = gtk_statusbar_new();
-  gtk_statusbar_push (GTK_STATUSBAR (StatusBar), 0,"NONO");
+  gtk_statusbar_push (GTK_STATUSBAR (StatusBar), 0,"NONO qui gère de ouf #dansedelajoie trololo");
     
   MenuBar = gtk_menu_bar_new();
   Menu = gtk_menu_new();
 
     //////////////////////MENU/////////////////////////  
    
-  MenuItem = gtk_menu_item_new_with_label("Choose");
+      
+  MenuItem =gtk_menu_item_new_with_mnemonic(("_Add Pictures"));
   gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
-  gtk_signal_connect(GTK_OBJECT (MenuItem), "activate", G_CALLBACK(choose), GTK_IMAGE(scre->image));  
-    
-   MenuItem =gtk_menu_item_new_with_mnemonic (("_Add Pictures"));
-  gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
-  gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",G_CALLBACK(Navigate),NULL);
+  gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",GTK_SIGNAL_FUNC(Navigate),NULL);
 
     
   MenuItem = gtk_menu_item_new_with_label("Supp");
@@ -130,7 +127,7 @@ int main (int argc, char** argv) {
   
   MenuItem = gtk_menu_item_new_with_label("Analyse");
   gtk_menu_shell_append(GTK_MENU_SHELL(Menu), MenuItem);
-  // gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",  GTK_SIGNAL_FUNC(open), NULL);//appel la detection
+  //gtk_signal_connect(GTK_OBJECT (MenuItem), "activate",  GTK_SIGNAL_FUNC(open), NULL);//appel la detection
   
   
   MenuItem = gtk_menu_item_new_with_label("Leave"); 
@@ -148,7 +145,8 @@ int main (int argc, char** argv) {
   gtk_menu_shell_append(GTK_MENU_SHELL(MenuBar), MenuItem);
   
   // Menu au dessus image//
-  gtk_box_pack_start(GTK_BOX(Image_box), MenuBar, FALSE, FALSE, 0);  gtk_box_pack_start(GTK_BOX(Image_box),scre->image,TRUE,FALSE,0);
+  gtk_box_pack_start(GTK_BOX(Image_box), MenuBar, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(Image_box),scre->image,TRUE,FALSE,0);
   gtk_box_pack_end(GTK_BOX(Image_box), StatusBar, FALSE, FALSE, 0);
 
   gtk_signal_connect(GTK_OBJECT (scre->Window), "destroy", GTK_SIGNAL_FUNC (Destroy), NULL);
--- N_GTK/Try_Image_GTK/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_H_
-#define TEST_H_
-#include <gtk/gtk.h>
-
-void file_ok_sel (GtkWidget *w, GtkFileSelection *fs);
-void destroy (GtkWidget *widget, gpointer *data);
-void yolo(GtkWidget to);
-# endif
